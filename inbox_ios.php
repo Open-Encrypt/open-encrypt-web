cf20ebd6d744274944bebae5daa88c53e5b262cf
Jackson Walters <jacksonwalters@gmail.com>
2024-09-16 12:04:01 -0400

    add get public key function

--- inbox_ios.php
+++ inbox_ios.php
@@ -56,34 +56,60 @@ function valid_secret_key($secret_key){
         return true;
     }
 }
-?>
-
-<?php
-//display all the encrypted messages sent to the current user
-if(isset($data['username']) && isset($data['token'])){
-    $username = $data['username'];
-    $token = $data['token'];
-    $secret_key = $data['secret_key'];
+//function to get messages from the database
+function get_messages($username,$conn,$secret_key,&$response){
     $valid_secret_key = valid_secret_key($secret_key);
-    if(verify_token($username,$conn,$token)){
-        $sql_get_messages = "SELECT * FROM messages WHERE `to` = '$username'";
-        try{
-            if ($result = mysqli_query($conn, $sql_get_messages)) {
-                $response['status'] = 'success';
-                while($row = $result->fetch_assoc()){
-                    array_push($response['from'],$row['from']);
-                    array_push($response['to'],$row['to']);
-                    if($valid_secret_key){
-                        array_push($response['messages'],decrypt_message($secret_key,$row['message']));
-                    }
-                    else{
-                        array_push($response['messages'],$row['message']);
-                    }
+    $sql_get_messages = "SELECT * FROM messages WHERE `to` = '$username'";
+    try{
+        if ($result = mysqli_query($conn, $sql_get_messages)) {
+            $response['status'] = 'success';
+            while($row = $result->fetch_assoc()){
+                array_push($response['from'],$row['from']);
+                array_push($response['to'],$row['to']);
+                if($valid_secret_key){
+                    array_push($response['messages'],decrypt_message($secret_key,$row['message']));
+                }
+                else{
+                    array_push($response['messages'],$row['message']);
                 }
             }
         }
+    }
+    catch(Exception $e) {
+        $response['error'] = "Error: " . $sql_get_messages . "|" . mysqli_error($conn);
+    }
+}
+//retrieve user's public key from database
+function get_public_key($username,$conn,&$response){
+    if(username_exists($username,"public_keys",$conn)){
+        $sql_select = "SELECT public_key FROM `public_keys` WHERE `username` = '$username'";
+        try{
+            if ($result = mysqli_query($conn, $sql_select)) {
+                $row = $result->fetch_assoc();
+                array_push($response['public_key'],$row['public_key']);
+            }
+        }
         catch(Exception $e) {
-            echo "Error: " . $sql_get_messages . "<br>" . mysqli_error($conn);
+            $response['error'] = "Error: " . $sql_insert . "|" . mysqli_error($conn);
+        }
+    }
+}
+?>
+
+<?php
+//check action variable and decide which SQL query to run
+if(isset($data['username']) && isset($data['token']) && isset($data['action'])){
+    $username = $data['username'];
+    $token = $data['token'];
+    $secret_key = $data['secret_key'];
+    $action = $data['action'];
+
+    if(verify_token($username,$conn,$token)){
+        if($action == "get_messages"){
+            get_messages($username,$conn,$secret_key,$response);
+        }
+        if($action == "get_public_key"){
+            get_public_key($username,$conn,$response);
         }
     }
 }
