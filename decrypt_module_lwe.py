209f4dad1e4d7d09909883eef02a99f8554f28ca
Jackson Walters <jacksonwalters@gmail.com>
2024-10-06 16:26:16 -0400

    finish decryption method for module-LWE

--- decrypt_module_lwe.py
+++ decrypt_module_lwe.py
@@ -40,11 +40,21 @@ if(len(argv) >  2):
     #get the ciphertext string and recover u, v
     ciphertext_string = argv[2]
     ciphertext_list = ciphertext_string.split(",")
-    u_array = np.int64(ciphertext_list[:k*n])
-    v_array = np.int64(ciphertext_list[k*n:])
-    u = np.reshape(u_array, (k,n))
-    v = np.reshape(v_array, (n))
-    #decrypt the ciphertext u, v with secret key s
-    m_b = decrypt(s, u, v, f, q) 
-    #export the decrypted message
-    print(m_b)
+    #break into blocks of size k*n+n = (k+1)*n
+    block_size = (k+1)*n
+    num_blocks = len(ciphertext_list) // block_size
+    message_binary = []
+    for i in range(num_blocks):
+      u_array = np.int64(ciphertext_list[i*block_size:i*block_size+k*n])
+      v_array = np.int64(ciphertext_list[i*block_size+k*n:(i+1)*block_size])
+      u = np.reshape(u_array, (k,n))
+      v = np.reshape(v_array, (n))
+      #decrypt the ciphertext u, v with secret key s
+      m_b = decrypt(s, u, v, f, q) 
+      #export the decrypted message
+      message_binary += m_b
+    # Group the bits back into bytes (8 bits each)
+    byte_chunks = [''.join(str(bit) for bit in message_binary[i:i+8]) for i in range(0, len(message_binary), 8)]
+    # Convert each binary string back into a character
+    message_string = ''.join([chr(int(byte, 2)) for byte in byte_chunks])
+    print(str(message_string).replace('[','').replace(']','').replace(' ',''))
