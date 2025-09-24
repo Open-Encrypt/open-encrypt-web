1c1a0b64c7d22c619ac56a085f924f1c16f0f125
Jackson Walters <jacksonwalters@gmail.com>
2025-09-23 20:16:25 -0400

    Revert "remove remaining python files"
    This reverts commit 94d390b5f857d60c3673cda1b933b4b7b8296fab.

---
+++ module_lwe.py
@@ -0,0 +1,56 @@
+from ring_lwe import polyadd, polymul
+import numpy as np
+
+def parameters():
+    # polynomial modulus degree
+    n = 2**2
+    # ciphertext modulus
+    q = 67
+    # polynomial modulus
+    poly_mod = np.array([1] + [0] * (n - 1) + [1])
+    #module rank
+    k = 2
+    return (n,q,poly_mod,k)
+
+def add_vec(v0, v1, q, f):
+  assert(len(v0) == len(v1)) # sizes need to be the same
+
+  result = []
+
+  for i in range(len(v0)):
+    result.append(polyadd(v0[i], v1[i], q, f))
+  
+  return result
+
+
+def mul_vec_simple(v0, v1, f, q):
+  assert(len(v0) == len(v1)) # sizes need to be the same
+
+  degree_f = len(f) - 1
+  result = [0 for i in range(degree_f - 1)]
+
+  # textbook vector inner product
+  for i in range(len(v0)):
+    result = polyadd(result, polymul(v0[i], v1[i], q, f), q, f)
+  
+  return result
+
+
+def mul_mat_vec_simple(m, a, f, q):
+  result = []
+  
+  # textbook matrix-vector multiplication
+  for i in range(len(m)):
+    result.append(mul_vec_simple(m[i], a, f, q))
+  
+  return result
+
+#transpose of a matrix
+def transpose(m):
+  result = [[None for i in range(len(m))] for j in range(len(m[0]))]
+
+  for i in range(len(m)):
+    for j in range(len(m[0])):
+      result[j][i] = m[i][j]
+  
+  return result
